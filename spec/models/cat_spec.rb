dd468c62c19fa2c68156139f45ee2dc1506615ca
AKFosterDev <akfoster2015@gmail.com>
2021-01-11 14:26:46 -0800

    add index and create request rspec and endpoint

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have a valid name' do
+    cat = Cat.create(age: 4, enjoys: 'eating', image: 'url address')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it 'should have a valid age' do
+    cat = Cat.create(name: 'Peaches', enjoys: 'eating', image: 'url address')
+    expect(cat.errors[:age]).to_not be_empty
+  end
+  
+  it 'should have a valid image' do
+    cat = Cat.create(name: 'Peaches',age: 4, enjoys: 'sleeping')
+    expect(cat.errors[:image]).to_not be_empty
+  end
+
 end
